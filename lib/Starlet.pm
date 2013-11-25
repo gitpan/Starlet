b08b7bdba62a9d02dd054a24fff874bd3447997b
Kazuho Oku <kazuhooku@gmail.com>
2013-11-25 07:52:32 +0000

    Import of KAZUHO/Starlet-0.21 from CPAN.
    gitpan-cpan-distribution: Starlet
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         KAZUHO/Starlet-0.21.tar.gz
    gitpan-cpan-author:       KAZUHO
    gitpan-cpan-maturity:     released

--- lib/Starlet.pm
+++ lib/Starlet.pm
@@ -2,7 +2,7 @@ package Starlet;
 
 use 5.008_001;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 1;
 __END__
@@ -80,6 +80,8 @@ L<Server::Starter>
 Kazuho Oku
 
 miyagawa
+
+kazeburo
 
 =head1 LICENSE
 
